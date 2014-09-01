dbc3d9e85fe28132830297215148eeae74e3814f
Julien Palard <julien@palard.fr>
2014-09-01 22:46:06 +0200

    A lot more verbose output when input is not UTF-8.

--- main.c
+++ main.c
@@ -6,10 +6,45 @@
 
 #define BUFSIZE 4096
 
+int showstr(const char *str, unsigned int max_length)
+{
+    size_t out;
+
+    out = 0;
+    while (*str)
+    {
+        if (max_length <= 0)
+            return out;
+        out += printf((*str < ' ' || *str > '~') ? "\\x%.2X" : "%c",
+                      (unsigned char)*str);
+        str++;
+        max_length -= 1;
+    }
+    return out;
+}
+
+
 static void pretty_print_error_at(char *str, int pos)
 {
-    str[pos] = '\0';
-    fprintf(stdout, "%s\n", str);
+    int chars_to_error;
+    char *error;
+
+    error = "^ This byte is not UTF8.";
+
+    if (pos > 10)
+    {
+        /* Ok, we got some context to print ... */
+        str = str + pos - 10; /* Print from 10 char before error. */
+        chars_to_error = showstr(str, 10); /* Print up to error. */
+        showstr(str + 10, 10); /* Print after error, to get context. */
+    }
+    else
+    {
+        /* Error is around the start of the string, we don't have context */
+        chars_to_error = showstr(str, pos); /* Print up to error. */
+        showstr(str + pos, 10); /* Print after error, to get context. */
+    }
+    printf("\n%*s\n", chars_to_error + strlen(error), error);
 }
 
 int main(int ac, char **av)
@@ -35,7 +70,6 @@ int main(int ac, char **av)
                 perror("read");
                 return EXIT_FAILURE;
             }
-
             if (is_utf8((unsigned char*)buffer, read_retval, &pos))
             {
                 pretty_print_error_at(buffer, pos);
@@ -43,7 +77,7 @@ int main(int ac, char **av)
             }
             else
             {
-                fprintf(stdout, "%s\n", buffer);
+                fprintf(stdout, "UTF-8 compliant ! Maybe ASCII !\n");
             }
         }
         return EXIT_SUCCESS;
--- test.sh
+++ test.sh
@@ -6,21 +6,34 @@ utf8_test()
     should_return="$2"
     should_print="$3"
 
-    answer="$(echo "$1" | ./is_utf8 -)"
+    answer="$(printf "$1" | ./is_utf8 -)"
     error_level=$?
     if ! [ z"$error_level" == z"$should_return" ]
     then
-        echo "Wrong error level, got $error_level should be $should_return"
+        printf "Wrong error level, got: %s should be: %s" $error_level $should_return
         exit 1
     fi
-    if ! [ z"$answer" == z"$should_print" ]
+    if [ ! -z "$3" ]
     then
-        echo "Wrong answer, got $answer, should be $should_print"
-        exit 1
+        if ! [ z"$answer" == z"$should_print" ]
+        then
+            printf "Wrong answer, got:\n%s\nshould be:\n%s\n" "$answer" "$should_print"
+            exit 1
+        fi
     fi
 }
 
-utf8_test "$(echo Привет$(echo мир | iconv -f utf-8 -t cp1251))" 1 "Привет"
-utf8_test "$(echo Привет$(echo мир | iconv -f utf-8 -t utf-8))" 0 "Приветмир"
+utf8_test "Léa" 0
+utf8_test "Maïté" 0
+utf8_test "Pauline" 0
+utf8_test "Élise" 0
+utf8_test $'\xc9\x6c\x69\x73\x65' 1 "\xC9lise
+^ This byte is not UTF8."
+utf8_test $'Elis\xc9' 1 "Elis\xC9
+    ^ This byte is not UTF8."
+
+utf8_test "$(echo Привет$(echo мир | iconv -f utf-8 -t cp1251))" 1 "\xD1\x80\xD0\xB8\xD0\xB2\xD0\xB5\xD1\x82\xEC\xE8\xF0
+                                        ^ This byte is not UTF8."
+utf8_test "$(echo Привет$(echo мир | iconv -f utf-8 -t utf-8))" 0 "UTF-8 compliant ! Maybe ASCII !"
 
 echo "All tests are OK."
