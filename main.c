f907c9faafeee5b84c32e04f3ac43f78e45fbf00
Julien Palard <julien@palard.fr>
2016-07-09 11:33:07 +0200

    Use a more predictible way to get a file vs a string.

--- main.c
+++ main.c
@@ -1,3 +1,6 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -16,7 +19,7 @@ int showstr(const char *str, unsigned int max_length)
         if (max_length <= 0)
             return out;
         out += fprintf(stderr, (*str < ' ' || *str > '~') ? "\\x%.2X" : "%c",
-                      (unsigned char)*str);
+                       (unsigned char)*str);
         str++;
         max_length -= 1;
     }
@@ -47,38 +50,41 @@ static void pretty_print_error_at(char *str, int pos, const char *message)
 int main(int ac, char **av)
 {
     char buffer[BUFSIZE];
-    int  read_retval;
+    int read_retval;
     int pos;
+    int source_fd;
     char *message;
 
     if (ac != 2)
     {
-        fprintf(stderr, "USAGE: %s STRING or - for stdin.\n", av[0]);
+        fprintf(stderr, "USAGE: %s FILE\n    Use '-' as a FILE to read from stdin.\n", av[0]);
         return EXIT_FAILURE;
     }
     if (strcmp(av[1], "-") == 0)
+        source_fd = 0;
+    else
+        source_fd = open(av[1], O_RDONLY);
+    if (source_fd == -1)
     {
-        while (42)
-        {
-            read_retval = read(0, buffer, BUFSIZE);
-            if (read_retval == 0)
-                return EXIT_SUCCESS;
-            if (read_retval == -1)
-            {
-                perror("read");
-                return EXIT_FAILURE;
-            }
-            pos = is_utf8((unsigned char*)buffer, read_retval, &message);
-            if (message != NULL)
-            {
-                pretty_print_error_at(buffer, pos, message);
-                return EXIT_FAILURE;
-            }
-        }
-        return EXIT_SUCCESS;
+        perror("open");
+        return EXIT_FAILURE;
     }
-    pos = is_utf8((unsigned char*)av[1], strlen(av[1]), &message);
-    if (message != NULL)
-        pretty_print_error_at(av[1], pos, message);
-    return message == NULL ? EXIT_SUCCESS : EXIT_FAILURE;
+    while (42)
+    {
+        read_retval = read(source_fd, buffer, BUFSIZE);
+        if (read_retval == 0)
+            return EXIT_SUCCESS;
+        if (read_retval == -1)
+        {
+            perror("read");
+            return EXIT_FAILURE;
+        }
+        pos = is_utf8((unsigned char*)buffer, read_retval, &message);
+        if (message != NULL)
+        {
+            pretty_print_error_at(buffer, pos, message);
+            return EXIT_FAILURE;
+        }
+    }
+    return EXIT_SUCCESS;
 }
